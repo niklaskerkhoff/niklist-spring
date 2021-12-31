cdd186f30b0c679e4809d747d8df5bba08cf3344
niklas <nskf2000@gmail.com>
2021-12-31 11:34:14 +0100

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.1"
+    id("org.springframework.boot") version "2.6.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.spring") version "1.6.0"
@@ -19,13 +19,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.1")
-    implementation("org.springframework.boot:spring-boot-starter-web:2.6.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.2")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.6.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("org.postgresql:postgresql:42.3.1")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.2")
 }
 
 tasks.withType<KotlinCompile> {
